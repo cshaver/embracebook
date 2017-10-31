61b3a34616b8a7310d65d205d004b1a5a3a6ba29
Cristina Shaver <cristina.shaver@gmail.com>
2017-10-31 04:30:44 -0700

    Formatting stuff

--- project.config.js
+++ project.config.js
@@ -2,27 +2,27 @@ const ip = require('ip')
 const NODE_ENV = process.env.NODE_ENV || 'development'
 
 module.exports = {
-  /** The environment to use when building the project */
+  // The environment to use when building the project
   env: NODE_ENV,
-  /** The full path to the project's root directory */
+  // The full path to the project's root directory
   basePath: __dirname,
-  /** The name of the directory containing the application source code */
+  // The name of the directory containing the application source code
   srcDir: 'src',
-  /** The file name of the application's entry point */
+  // The file name of the application's entry point
   main: 'main',
-  /** The name of the directory in which to emit compiled assets */
+  // The name of the directory in which to emit compiled assets
   outDir: 'dist',
-  /** The base path for all projects assets (relative to the website root) */
+  // The base path for all projects assets (relative to the website root)
   publicPath: NODE_ENV === 'development' ? `http://${ip.address()}:3000/` : '/',
-  /** Whether to generate sourcemaps */
-  sourcemaps: true,
-  /** A hash map of keys that the compiler should treat as external to the project */
+  // Whether to generate sourcemaps
+  sourcemaps: NODE_ENV === 'development',
+  // A hash map of keys that the compiler should treat as external to the project
   externals: {},
-  /** A hash map of variables and their values to expose globally */
+  // A hash map of variables and their values to expose globally
   globals: {},
-  /** Whether to enable verbose logging */
+  // Whether to enable verbose logging
   verbose: false,
-  /** The list of modules to bundle separately from the core application code */
+  // The list of modules to bundle separately from the core application code
   vendors: [
     'react',
     'react-dom',
@@ -44,8 +44,11 @@ module.exports = {
     storageBucket: 'embracebook-e403d.appspot.com'
   },
   reduxFirebase: {
-    userProfile: 'users', // root that user profiles are written to
-    enableLogging: false, // enable/disable Firebase Database Logging
-    updateProfileOnLogin: false // enable/disable updating of profile on login
+    // root that user profiles are written to
+    userProfile: 'users',
+    // enable/disable Firebase Database Logging
+    enableLogging: false,
+    // enable/disable updating of profile on login
+    updateProfileOnLogin: false
   }
 }
