23eb30fec7d86687f2478438e7472b518eae33bf
Cristina Shaver <cristina.shaver@gmail.com>
2017-12-13 00:15:39 -0800

    Rename route folders

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,13 @@
 // We only need to import the modules necessary for initial render
 import Layout from 'components/Layout'
-import Home from './Home'
-import LoginRoute from './Login'
-import InviteRoute from './Invite'
-import ProfileListRoute from './Profile'
-import NPCsRoute from './NPCs'
-import PlayersRoute from './Players'
-import AccountRoute from './Account'
-import NotFoundRoute from './NotFound'
-import NoAccessRoute from './NoAccess'
+import Feed from './feed'
+import LoginRoute from './login'
+import ProfileRoute from './profile'
+import NPCsRoute from './npcs'
+import PlayersRoute from './players'
+import AccountRoute from './account'
+import NotFoundRoute from './not-found'
+import NoAccessRoute from './no-access'
 
 /*  Note: Instead of using JSX, we recommend using react-router
     PlainRoute objects to build route definitions.   */
@@ -16,16 +15,15 @@ import NoAccessRoute from './NoAccess'
 export const createRoutes = store => ({
   path: '/',
   component: Layout,
-  indexRoute: Home,
+  indexRoute: Feed,
   childRoutes: [
     AccountRoute,
     LoginRoute,
-    InviteRoute,
     NoAccessRoute(store),
     // async route definitions recieve store
     NPCsRoute(store),
     PlayersRoute(store),
-    ProfileListRoute(store),
+    ProfileRoute(store),
     // Place all Routes above here so NotFoundRoute can act as a 404 page
     NotFoundRoute(store)
   ]
